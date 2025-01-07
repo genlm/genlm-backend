c29a218dbad36044d61b04b43530c8405248241f
benlebrun <benlebrun1@gmail.com>
2025-01-07 12:34:18 -0500

    rm util

--- genlm_backend/util.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def resolve_device(device):
-    if not device:
-        return 'cuda' if torch.cuda.is_available() else 'cpu'
-    if device not in ['cpu', 'cuda']:
-        raise ValueError(f"Invalid device: {device}. Must be 'cpu', 'cuda' or None")
-    return device
