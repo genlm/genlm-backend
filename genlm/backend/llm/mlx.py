32ab601c5f9602617446bf7a8ee9b9dc7a81346c
shepardxia <78109174+shepardxia@users.noreply.github.com>
2025-09-15 16:10:36 -0400

    added naive logprobs and sampling methods

--- genlm/backend/llm/mlx.py
+++ genlm/backend/llm/mlx.py
@@ -4,6 +4,12 @@ import warnings
 
 from genlm.backend.llm.base import AsyncLM
 from genlm.backend.cache import OutputCache
+import mlx_lm
+from mlx_lm.generate import generate_step
+import mlx.core as mx
+import asyncio
+from mlx_lm.sample_utils import make_sampler
+
 
 """ try:
     from vllm import AsyncLLMEngine, SamplingParams, AsyncEngineArgs
@@ -58,7 +64,7 @@ else:
 
             """
             
-            self.async_llm_engine = mlx_lm_model
+            self.mlx_lm_model = mlx_lm_model
             self.tokenizer = tokenizer
             #self.request_counter = Counter()
             self.cache = (
@@ -113,6 +119,7 @@ else:
         #def underlying_model(self):
         #    return self.async_llm_engine.engine.model_executor.driver_worker.model_runner.model
 
+
         async def next_token_logprobs(self, token_ids):
             """Request log probabilities of next token asynchronously with output caching.
 
@@ -126,46 +133,8 @@ else:
                 Do not use `asyncio.run(next_token_logprobs())` as it may interfere with vLLM's background loop.
                 For synchronous usage, use the `next_token_logprobs_sync()` method instead.
             """
-            key = tuple(token_ids)
+            return self.next_token_logprobs_sync(token_ids)
 
-            if self.cache is not None and key in self.cache:
-                return self.cache[key]
-
-            result = await self._next_token_logprobs(key)
-
-            if self.cache is not None:
-                self.cache[key] = result
-
-            return result
-
-        async def _next_token_logprobs(self, token_ids):
-            """Request log probabilities of next token asynchronously.
-
-            Args:
-                token_ids_list (list[int]): A list of token IDs, representing a prompt to the language model.
-
-            Returns:
-                (torch.Tensor): Normalized log probability tensor.
-            """
-            req_id = str(next(self.request_counter))
-            prompt = TokensPrompt(prompt_token_ids=token_ids)
-
-            outputs = []
-            processor = PassThroughLogitsProcessor()
-            async for output in self.async_llm_engine.generate(
-                prompt=prompt,
-                sampling_params=SamplingParams(
-                    **self.default_params, logits_processors=[processor]
-                ),
-                request_id=req_id,
-            ):
-                if output.finished:
-                    outputs.append(output)
-
-            assert processor.log_probs is not None, (
-                "Log probs should be set by the logits processor."
-            )
-            return processor.log_probs
 
         def next_token_logprobs_sync(self, token_ids):
             """Request log probabilities of next token synchronously.
@@ -176,7 +145,21 @@ else:
             Returns:
                 (torch.Tensor): Normalized log probability tensor.
             """
-            return self.batch_next_token_logprobs_sync([token_ids])[0]
+            key = tuple(token_ids)
+
+            if self.cache is not None and key in self.cache:
+                return self.cache[key]
+
+            token_ids_array = mx.array(token_ids)
+            tok_gen = generate_step(token_ids_array, self.mlx_lm_model, max_tokens=1)
+            _, logprobs = next(tok_gen)
+
+            if self.cache is not None:
+                self.cache[key] = torch.log_softmax(torch.tensor(logprobs), dim=-1)
+
+
+            return torch.log_softmax(torch.tensor(logprobs), dim=-1)
+
 
         def batch_next_token_logprobs_sync(self, token_ids_list):
             """
@@ -188,32 +171,11 @@ else:
             Returns:
                 (torch.Tensor): A tensor of normalized log probability tensors, one for each prompt in the input list.
             """
-            req_ids = []
-            req_id2processors = {}
+            outputs = []
             for token_ids in token_ids_list:
-                req_id = str(next(self.request_counter))
-                req_ids.append(req_id)
-                processor = PassThroughLogitsProcessor()
-                req_id2processors[req_id] = processor
-                self.async_llm_engine.engine.add_request(
-                    prompt=TokensPrompt(prompt_token_ids=token_ids),
-                    params=SamplingParams(
-                        **self.default_params, logits_processors=[processor]
-                    ),
-                    request_id=req_id,
-                )
+                outputs.append(self.next_token_logprobs_sync(token_ids))
+            return torch.stack(outputs)
 
-            while self.async_llm_engine.engine.has_unfinished_requests():
-                output = self.async_llm_engine.engine.step()
-                for out in output:
-                    if out.finished:
-                        assert out.request_id in req_id2processors, (
-                            f"{out.request_id} not in requested IDs"
-                        )
-
-            return torch.stack(
-                [req_id2processors[req_id].log_probs for req_id in req_ids]
-            )
 
         def clear_cache(self):
             """Clear output cache."""
@@ -225,11 +187,9 @@ else:
             self._cleanup_engine()
 
         def _cleanup_engine(self):
-            """Clean up the vLLM engine and associated resources."""
-            if async_engine := getattr(self, "async_llm_engine", None):
-                async_engine.shutdown_background_loop()
-                destroy_model_parallel()
-                destroy_distributed_environment()
+            """Clean up the MLX LM engine and associated resources."""
+            pass
+            
 
         async def sample(
             self,
@@ -251,7 +211,11 @@ else:
             Returns:
                 (list[int]): The sampled token IDs.
             """
-            async for output in self.async_llm_engine.generate(
+
+            if seed is not None:
+                mx.random.seed(seed)
+
+            """ async for output in self.async_llm_engine.generate(
                 prompt=TokensPrompt(prompt_token_ids=prompt_token_ids),
                 sampling_params=SamplingParams(
                     n=1,
@@ -269,4 +233,14 @@ else:
                     token_ids = list(output.outputs[0].token_ids)
                     if token_ids[-1] in eos_token_ids:
                         token_ids = token_ids[:-1]
-                    return token_ids
+                    return token_ids """
+
+            sampler = make_sampler(temp=temperature)
+            prompt_token_ids_array = mx.array(prompt_token_ids)
+            token_generator = generate_step(prompt_token_ids_array, self.mlx_lm_model, max_tokens=max_tokens, sampler=sampler)
+            generated_token_ids = []
+            for sampled, _ in token_generator:
+                if sampled in eos_token_ids:
+                    break
+                generated_token_ids.append(sampled)
+            return generated_token_ids
